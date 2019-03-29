ea332d771034c04d1d26a8aef631c379074771a7
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2019-03-29 15:36:26 +0700

    Added scripts to control menu & floating btn behaviour

--- src/js/vendor.js
+++ src/js/vendor.js
@@ -8,6 +8,7 @@ $(document).ready(() => {
         initialSlide: 0,
         autoHeight: true,
         mousewheel: true,
+        sensitivity: 2,
         forceToAxis: true,
         releaseOnEdges: true,
         simulateTouch: false,
@@ -17,8 +18,37 @@ $(document).ready(() => {
             1024: {
                 autoHeight: true,
                 keyboard: false,
-                enabled: false,
+                enabled: false
+            }
+        },
+        on: {
+            transitionEnd() {
+                const btnClass = scrollTopBtn.attr('class').split(' ')[0];
+                if (this.realIndex === this.slides.length - 1) {
+                    scrollTopBtn.show();
+                } else {
+                    scrollTopBtn.hide();
+                }
             }
         }
+    });
+    const toggleMenuBtn = $('.toggle-menu-btn');
+    const hideMenuBtn = $('.hide-menu-btn');
+    const menuElem = $('.site-menu-container');
+    const menuElemBaseClass = menuElem.attr('class');
+    const scrollTopBtn = $('.floating-action-btn');
+    // Поведение меню
+    $('.page-navigation').on('click', 'a',
+            (event) => {
+                if (event.currentTarget === toggleMenuBtn[0]) {
+                    menuElem.toggleClass(`${menuElemBaseClass}--shown`);
+                }
+                if (event.currentTarget === hideMenuBtn[0]) {
+                    menuElem.toggleClass(`${menuElemBaseClass}--shown`);
+                }
+            })
+        // Поведение дял кнопки прокрутки в начало
+    scrollTopBtn.on('click', (event) => {
+        swiper.slideTo(0, 300);
     })
-})
+});
